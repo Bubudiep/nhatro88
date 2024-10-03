22b1523bbba539dd4718e805bd9dd3d899acfaf3
Bubudiep <hlitdvp98@gmail.com>
2024-10-03 23:56:42 +0700

    update scroll

--- src/components/user-card.jsx
+++ src/components/user-card.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import bg_night from "../img/bg_city.png";
-import bg_day from "../img/bg_day.png";
 const UserCard = ({ user }) => {
-  const hour = new Date().getHours();
-  const pageStyle = {
-    backgroundImage: `url(${hour > 16 ? bg_night : bg_day})`,
-  };
   return (
-    <div className="user-card" style={pageStyle}>
+    <div className="user-card">
       <div className="user-info">
         <div className="avatar">
           <img src={user.zalo.userInfo.avatar} />
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,18 @@ import React, { useState, useEffect, useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import logo from "../../img/logo.png";
 import UserCard from "../../components/user-card";
+import bg_night from "../../img/home_city_night.png";
+import bg_day from "../../img/bg_day.png";
 const Home = ({ user }) => {
+  const hour = new Date().getHours();
+  const pageStyle = {
+    backgroundImage: `url(${hour > 16 ? bg_night : bg_day})`,
+  };
   useEffect(() => {
     console.log(user);
   }, [user]);
   return (
-    <div className="home-page">
+    <div className="home-page" style={pageStyle}>
       <UserCard user={user} />
     </div>
   );
